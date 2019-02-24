fce56c55f7d28724f2e1ab8bb1742343b6208d81
ECE.sohn <ECE.sohn@wolftech.ad.ncsu.edu>
2019-02-23 23:15:52 -0500

    comments are added to the main programs

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,10 +1,10 @@
 import os
 import csv
 
+#------------  Import/Read the csv dataset  -------------------------------------------
 CSVpath = os.path.join('.', 'budget_data.csv')
 date=[]
 profit=[]
-#------------------------------------------------
 with open(CSVpath, 'r') as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
     header = next(csvreader) 
@@ -12,6 +12,7 @@ with open(CSVpath, 'r') as csvfile:
         date.append(row[0])
         profit.append(int(row[1]))
 
+#------- create a list of diff (monthly change of profit) and calculation --------------
 diff=[]
 length=len(profit)
 t_months=1 # since I am not counting the last month inthe for loop later
@@ -19,41 +20,33 @@ net_t_amt = 0
 for k in range(length-1):
     t_months += 1
     net_t_amt += int(profit[k])  # net total amt    
-    i=k+1
+    i=k+1                        
     d_diff=profit[i]-profit[i-1]
     diff.append(d_diff)
-net_t_amt+=int(profit[length-1]) # since i am not count the last month's amt
-
-#----------------------------------------------------------------
+net_t_amt+=int(profit[length-1]) # adding the last month's profit
 avg_change=round((profit[t_months-1]-profit[0])/(t_months-1),2)
-#----------------------------------------------------------------
 
+#--------- create a dataset with diff and date ---_________________________----------------
 del date[0]
 result={i:j for i,j in zip(diff,date)}
 s_result=dict(sorted(result.items(), reverse=True))
 
-print("Financial Analysis")
-print('----------------------------')
-print(f" total months   : {str(t_months)}")
-print(f"net total amount: {str(net_t_amt)}")
-print(f"average change  : {str(avg_change)}")
-print(f"greatest increase in profit: {max(diff)}  {str(s_result[max(diff)])}")
-print(f"greatest decrease in profit: {min(diff)}  {str(s_result[min(diff)])}") 
+#---------- print the analysis to the terminal -------------------------------------------
+print("\n Financial Analysis")
+print('-----------------------------------------------')
+print(f"Total Months    :  {str(t_months)}")
+print(f"Total           : ${str(net_t_amt)}")
+print(f"Average Change  : ${str(avg_change)}")
+print(f"Greatest Increase in Profits:  {str(s_result[max(diff)])}  ($ {max(diff)})  ")
+print(f"Greatest Decrease in Profits:  {str(s_result[min(diff)])}  (${min(diff)})\n") 
 
-#------------------------------------------------------------------------
-csv_path = os.path.join(".", "PyBank_stat.csv")
-with open(csv_path, 'w', newline='') as csvfile:
-    #csvwriter = csv.writer(csvfile, delimiter=',')
-    csvfile.write("Financial Analysis")
-    csvfile.write("\n")
-    csvfile.write(f'----------------------------')
-    csvfile.write("\n")
-    csvfile.write(f'Total Months: {str(t_months)}')
-    csvfile.write("\n")
-    csvfile.write(f'Total: {str(net_t_amt)}')
-    csvfile.write("\n")
-    csvfile.write(f'Average  Change: {str(avg_change)}')
-    csvfile.write("\n")
-    csvfile.write(f'Greatest Increase in Profits: {str(max(diff))}  {str(s_result[max(diff)])}')
-    csvfile.write("\n")
-    csvfile.write(f'Greatest Decrease in Profits: {str(min(diff))}  {str(s_result[min(diff)])}') 
+#--------- export a text file with the results -----------------------------
+csv_path = os.path.join(".", "PyBank_stat.txt")
+with open(csv_path, 'w', newline='') as txtfile:
+    txtfile.write("Financial Analysis \n")
+    txtfile.write(f'--------------------------------------------------\n')
+    txtfile.write(f'Total Months   : {str(t_months)}    \n')
+    txtfile.write(f'Total          : ${str(net_t_amt)}  \n')
+    txtfile.write(f'Average  Change: ${str(avg_change)} \n')
+    txtfile.write(f'Greatest Increase in Profits: {str(s_result[max(diff)])}  ($ {max(diff)}) \n')
+    txtfile.write(f'Greatest Decrease in Profits: {str(s_result[min(diff)])}  (${min(diff)})   \n') 
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,24 +1,23 @@
 import os
 import csv
 
+#------------ Import/Read the csv dataset--------------------------
 CSVpath = os.path.join('.', 'election_data.csv')
-
 candidate=[]
-#------------------------------------------------
 with open(CSVpath, 'r') as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
     header = next(csvreader) 
     can_count=0
     for row in csvreader:
         candidate.append(row[2])
-t_votes=len(candidate)
-#--------------------------------------------------------------------------
+t_votes=len(candidate) # total number of votes cast
+
+#------- create 3 lists of name, votes, and percent/ calculation -----------
 s_cand=sorted(candidate)
-e_vote=1
+e_vote=1  # total number of votes per each candidate
 name=[]
 votes=[]
 percent=[]
-k=0
 for i in range(len(s_cand)-1):
     if s_cand[i] != s_cand[i+1]:
           win_percent=format(round(e_vote/t_votes*100),'.3f')
@@ -26,40 +25,35 @@ for i in range(len(s_cand)-1):
           votes.append(e_vote)
           percent.append(win_percent)
           e_vote=1
-          k+=1
     else:
          e_vote += 1
 win_percent=format(round(e_vote/t_votes*100),'.3f')    
 name.append(s_cand[i])
 votes.append(e_vote)
 percent.append(win_percent)
+ 
+result={i:[j,k] for i,j,k in zip( votes,name, percent)} # zip the 3 variables
+s_votes=sorted(votes, reverse=True)                     # sorting in the descending order
 
-#----------------------------------------------------------------------------   
-result={i:[j,k] for i,j,k in zip( votes,name, percent)}
-s_votes=sorted(votes, reverse=True)
-
-print(f' -------------------------')
+#--------- print the analysis to the terminal ---------------------------  
 print(f' Election Results ')
-print(f' -------------------------')
+print(f'----------------------------')
 print(f' Total Votes: {str(t_votes)} ')
-print(f' -------------------------')
+print(f'----------------------------')
 for i in s_votes:
     print(f' {str(result[i][0])} : {str(result[i][1])}%  ({str(i)}) ')
-print(f' -------------------------')
+print(f'----------------------------')
 print(f'  Winner : {result[max(votes)][0]}')
-print(f' -------------------------')
+print(f'----------------------------')
 
-#------------------------------------------------------------------------
+#---------- export a text file with the results ---------------------------
 csv_path = os.path.join(".", "PyPoll_stat.txt")
-with open(csv_path, 'w', newline='') as csvfile:
-    #csvwriter = csv.writer(csvfile, delimiter=',')
-    csvfile.write("   Election Results \n")
-    csvfile.write('----------------------------\n')
-    csvfile.write(f'Total Votes: {str(t_votes)} \n')
+with open(csv_path, 'w', newline='') as txtfile:
+    txtfile.write("   Election Results \n")
+    txtfile.write('----------------------------\n')
+    txtfile.write(f' Total Votes: {str(t_votes)} \n')
     for i in s_votes:
-        csvfile.write(f' {str(result[i][0])} : {str(result[i][1])}%  ({str(i)}) \n')
-    csvfile.write(f' -------------------------\n')
-    csvfile.write(f'  Winner : {result[max(votes)][0]} \n')
-    csvfile.write(f' -------------------------')
-
- 
+        txtfile.write(f' {str(result[i][0])} : {str(result[i][1])}%  ({str(i)}) \n')
+    txtfile.write('----------------------------\n')
+    txtfile.write(f'  Winner : {result[max(votes)][0]} \n')
+    txtfile.write('----------------------------\n')
